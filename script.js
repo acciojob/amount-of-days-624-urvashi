9312cc529b9dffc6968b04688490598a55b031ba
624-urvashi <143706170+624-urvashi@users.noreply.github.com>
2025-01-02 16:24:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-//your JS code here. If required.
+function daysOfAYear(year) {
+    let days; 
+    
+    if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+        days = 366; 
+    } else {
+        days = 365;
+    }
+
+    return days; 
+}
+
+// Examples
+console.log(daysOfAYear(2022)); // Output: 365
+console.log(daysOfAYear(2024)); // Output: 366
+console.log(daysOfAYear(1900)); // Output: 365
+console.log(daysOfAYear(2000)); // Output: 366
